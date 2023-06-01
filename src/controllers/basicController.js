0161877095be9b661b5122df09474f19665b5f4b
pablo.scalora <pablo.scalora@gmail.com>
2023-05-31 21:32:01 -0300

    se elimina el directorio node_modules del controlador de versiones y se modifica el sistema de ruteo

--- src/controllers/basicController.js
+++ src/controllers/basicController.js
@@ -1,21 +1,21 @@
 const basicController = {
     home: (req, res) => {
-        res.render('./basic/home');
+        res.render('home');
     },
     login: (req, res) => {
-        res.render('./basic/home');
+        res.render('users/login');
     },
     register: (req, res) => {
-        res.render('./basic/home');
+        res.render('users/register');
     },
     productDetail: (req, res) => {
-        res.render('./basic/home');
+        res.render('products/productDetail');
     },
     productCart: (req, res) => {
-        res.render('./basic/home');
+        res.render('productCart');
     },
     whisList: (req, res) => {
-        res.render('./basic/home');
+        res.render('productWhisList');
     }
 };
 
