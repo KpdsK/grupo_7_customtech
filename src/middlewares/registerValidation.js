9e0239f535588aa52be0da64901ef97091fef6a3
0n3try <alva1432@gmail.com>
2023-07-23 19:21:04 -0300

    corrgiendo error de login

--- src/middlewares/registerValidation.js
+++ src/middlewares/registerValidation.js
@@ -2,7 +2,7 @@ const { body } = require('express-validator');
 
 module.exports = [ 
 
-  body('contrasenia').isStrongPassword({ minLength: 5, minUppercase: 1, minLowercase: 4, minSymbols: 1, minNumbers: 3 }).withMessage('La contraseña no puede estar vacia'),
+  body('contrasenia').isStrongPassword({ minLength: 5, minUppercase: 1, minLowercase: 4, minSymbols: 1, minNumbers: 1 }).withMessage('La contraseña no puede estar vacia'),
   body('email').notEmpty().withMessage('Indicá un email')
 
 ]
