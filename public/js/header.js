aa0e1f4a4cd4d3cefb7bf8a74e6b13dd35608dc6
0n3try <alva1432@gmail.com>
2023-09-25 21:09:37 -0300

    subiendo el menu hamburguesa

--- public/js/header.js
+++ public/js/header.js
@@ -1,10 +1,9 @@
-// console.log('hola mundo')  testeo
-// console.log(document.querySelector('.header-img'));
+
 window.onload  =() => {
 
   const hidden = document.querySelector('.menu-hidden');
   const burger = document.querySelector('.burger-menu');
-  const login = document.querySelector('.login');
+  const login = document.querySelector('.login-menu-list');
   const modal = document.querySelector('.login-modal')
 
   burger.addEventListener('click' , () => {
