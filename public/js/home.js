bac69826c013e3824615bf8eac83de0374aff523
0n3try <alva1432@gmail.com>
2023-09-28 20:33:10 -0300

    arreglando error del header y estilos

--- public/js/home.js
+++ public/js/home.js
@@ -1,24 +1,4 @@
 window.onload  =() => {
-
-  const headerLogin = document.querySelector('.image-header-login');
-  const modalHeader = document.querySelector('.modal-header')
-  const hidden = document.querySelector('.menu-hidden');
-  const burger = document.querySelector('.burger-menu');
-  const login = document.querySelector('.login-burger');
-  const modal = document.querySelector('.login-modal')
-
-
-  burger.addEventListener('click' , () => {
-    hidden.classList.toggle('show')
-
-  })
-  headerLogin.addEventListener('click', () => {
-    modalHeader.classList.toggle('mirar')
-  })
-
-  login.addEventListener('click', () => {
-    modal.classList.toggle('mirar');
-  })
   
 
     var slideIndex = 1;
